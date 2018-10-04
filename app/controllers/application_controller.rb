500464feb67128500b698aaf9daa3294f507ddef
thaotonto <ots.hunter@gmail.com>
2018-10-05 00:47:23 +0700

    sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location!, if: :storable_location?
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  protected
+
+  def storable_location?
+    request.get? && is_navigational_format? &&
+      !devise_controller? && !request.xhr?
+  end
+
+  def store_current_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
+    devise_parameter_sanitizer.permit :account_update, keys: [:name]
+  end
 end
