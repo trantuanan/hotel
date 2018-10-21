ef1481decc75bad4cca368289e422027192a1f8a
thaotonto <ots.hunter@gmail.com>
2018-10-21 22:28:08 +0700

    Revert "add homepage and detail page"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /ja|en/ do
     devise_for :users
     root "static_pages#home"
-    resources :static_pages
     resources :motels
     resources :rooms
     resources :equipments
